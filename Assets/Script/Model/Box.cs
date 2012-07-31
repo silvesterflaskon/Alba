09d1be1c4e00564dd7478f20d088cff9f8fe8be9
silvester <silvester@flaskon.com>
2012-07-31 14:38:28 +0900

    프리팹, 모델 업데이트

--- Assets/Script/Model/Box.cs
+++ Assets/Script/Model/Box.cs
@@ -1,18 +1,57 @@
-using UnityEngine;
+using System;
 using System.Collections;
+using UnityEngine;
 
 public class Box : MonoBehaviour {
-	public UILabel boxLabel;
+	public TextMesh boxLabel;
+	public GameObject boxModel;	
 	public int start;
 	public int destination;
+	public bool isShowed;
+	
+	protected Transform OriginalPos;
+	
+	protected int boxFingerID = -1;
 	
 	// Use this for initialization
-	void Start () {
-		boxLabel.text = start;
+	void Start () 
+	{
+		boxLabel.text = start.ToString();
+		OriginalPos = boxModel.transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+//		if(isShowed)
+//		{
+			boxLabel.text = start.ToString();
+//		}
+//		else
+//		{
+//			boxLabel.text = "";
+			
+			foreach (Touch touch in Input.touches)
+			{
+				if (touch.phase == TouchPhase.Began)
+				{
+					boxFingerID = touch.fingerId;
+					boxModel.transform.position = touch.position;				
+
+				} else if (touch.phase == TouchPhase.Stationary)
+				{
+					boxFingerID = touch.fingerId;
+					boxModel.transform.position = touch.position;				
+				} else if (touch.phase == TouchPhase.Moved)
+				{
+					boxFingerID = touch.fingerId;
+					boxModel.transform.position = touch.position;				
+				} else if (touch.phase == TouchPhase.Ended)
+				{
+					boxFingerID = touch.fingerId;
+					boxModel.transform.position = touch.position;				
+				}
+			}
+//		}
 	}
 }
